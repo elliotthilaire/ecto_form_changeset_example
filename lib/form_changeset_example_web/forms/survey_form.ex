c32f5e6fea765702e58e1e5102724e87d409b42a
Elliott Hilaire <elliott.hilaire@gmail.com>
2018-02-15 18:22:58 +0000

    Add custom validation

--- lib/form_changeset_example_web/forms/survey_form.ex
+++ lib/form_changeset_example_web/forms/survey_form.ex
@@ -18,5 +18,19 @@ defmodule FormChangesetExampleWeb.SurveyForm do
     survey
     |> cast(attrs, [:name, :favourite_animal])
     |> validate_required([:name, :favourite_animal])
+    |> validate_is_dog(:favourite_animal)
+  end
+
+  defp validate_is_dog(changeset, field) do
+    changeset
+    |> validate_change(field, &dog_validator/2)
+  end
+
+  defp dog_validator(field, value) do
+    if value == "dog" do
+      []
+    else
+      [{field, "must be dog"}]
+    end
   end
 end
